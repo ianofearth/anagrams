e1b97f473605caec4692dc63b732b8caf96aa622
Steven Bussert & Ian Bevel <student@epicodus.com>
2015-04-23 16:52:27 -0700

    added an additional spec

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,6 +3,8 @@ require('anagrams')
 
 describe('Array#anagrams') do
   it('will take an input and detirmine if a second input is an anagram of that word') do
+    expect("dog".anagrams(["god"])).to(eq(["god"]))
+  it('will take an input and detirmine if a second array of inputs includes anagrams of that word') do
     expect("dog".anagrams(["god", "sap", "go", "doggie", "odg", "pic"])).to(eq(["god", "odg"]))
   end
 
